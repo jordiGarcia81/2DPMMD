f153daf4da52a8529b5b9317a34a10451a9540b6
jordiGarcia <jordigarcia81@hotmail.com>
2020-09-24 13:03:07 +0100

    subimos ejercicio 6

---
+++ ejercicio6.js
@@ -0,0 +1,12 @@
+//ejercicio 6:
+
+num=prompt("introduzca un numero: ");
+function functionParImpar(num){
+    if(num%2==0){
+        return "el numero es par";
+    }else{
+        return "el numero es impar"
+    }
+}
+
+console.log(functionParImpar(num));
