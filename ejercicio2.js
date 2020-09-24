6c6d7d7847222cc9446cf3acf0e3413d7bd2f5a2
jordiGarcia <jordigarcia81@hotmail.com>
2020-09-24 12:40:24 +0100

    subimos ejercicio 2

---
+++ ejercicio2.js
@@ -0,0 +1,8 @@
+// ejercicio 2:
+
+let meses=["enero","feberero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"];
+for(let i=0;i<meses.length;i++){
+    console.warn(meses[i]);
+    
+}
+console.table(meses);
